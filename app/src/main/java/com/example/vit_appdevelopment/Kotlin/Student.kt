45a1c24749a9890d17d6bc88ff280a9c83843087
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-07 10:27:57 +0530

    Convert Java to Kotlin

---
+++ app/src/main/java/com/example/vit_appdevelopment/Kotlin/Student.kt
@@ -0,0 +1,4 @@
+package com.example.vit_appdevelopment.Kotlin
+
+// pojo - plain old java object / vo value object / model class - box with partitions
+class Student(var name: String, var age: Int, var postalAddress: String)
