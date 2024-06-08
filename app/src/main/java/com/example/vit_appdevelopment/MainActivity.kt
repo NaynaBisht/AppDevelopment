65dd154fc0b50715baf16cfe1934d617da2362e8
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-08 11:43:42 +0530

    webIntent

--- app/src/main/java/com/example/vit_appdevelopment/MainActivity.kt
+++ app/src/main/java/com/example/vit_appdevelopment/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.vit_appdevelopment
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -25,6 +26,9 @@ class MainActivity : AppCompatActivity() {
         Log.i("MainActivity-clickHandler", "button clicked")
 //        intent = intention
         var dialIntent: Intent = Intent(Intent.ACTION_DIAL)
-        startActivity(dialIntent)
+       startActivity(dialIntent)
+
+        var webIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
+        startActivity(webIntent)
     }
 }
