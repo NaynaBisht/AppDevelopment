344da57c12c844eade2a71b8e0fb1e985379e286
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-20 14:41:30 +0530

    lifecycle of an activity

--- app/src/main/java/com/example/vit_appdevelopment/MainActivity.kt
+++ app/src/main/java/com/example/vit_appdevelopment/MainActivity.kt
@@ -6,22 +6,47 @@ import android.os.Bundle
 import android.provider.AlarmClock
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
+    var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "activity is getting created")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+//    activity is visible to user
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "activity is started")
+    }
+
+//    activity has paused
+    override fun onPause() {
+        super.onPause()
+    Log.w(TAG, "activity is paused")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "activity is getting stopped")
+    }
+//    activity no longer exists
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(TAG, "activity is destroyed")
     }
 
     fun clickHandler(view: View) {
@@ -50,4 +75,16 @@ class MainActivity : AppCompatActivity() {
         }
         startActivity(intent)
     }
+
+
+    fun inflateXml(){
+        var nameEditText = EditText(this)
+        nameEditText.setHint("enter your name")
+        var pwdEditText = EditText(this)
+        pwdEditText.setHint("enter your pass")
+        var loginButton = Button(this)
+        loginButton.setText("login")
+    }
+
+
 }
