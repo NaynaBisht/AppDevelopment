e2a77c5684c888c9dec4ddaae9297f2ea88655b8
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-26 09:14:42 +0530

    recyclerview part1 - xml, data,handle

--- app/src/main/java/com/example/vit_appdevelopment/HomeActivity.kt
+++ app/src/main/java/com/example/vit_appdevelopment/HomeActivity.kt
@@ -11,14 +11,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
-class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
-    AdapterView.OnItemClickListener {
+class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
 
+    var data = arrayOf("india","hindi","blue")
     var TAG = HomeActivity::class.java.simpleName
 
     lateinit var mySpinner: Spinner
-    lateinit var myListView: ListView
+//    lateinit var myListView: ListView
+    lateinit var myRecycler: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +28,9 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
         setContentView(R.layout.activity_home)
 
         mySpinner = findViewById(R.id.spinner)
-        myListView = findViewById(R.id.listView)
-        myListView.isClickable = true
+//        myListView = findViewById(R.id.listView)
+//        myListView.isClickable = true
+        myRecycler = findViewById(R.id.recyclerView)
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -37,7 +40,7 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
         }
 
         mySpinner.onItemSelectedListener = this
-        myListView.setOnItemClickListener(this)
+//        myListView.setOnItemClickListener(this)
 
 //        var data = intent.extras?.getString("nkey")
 //        Log.i("HomeActivity", "data is ="+data)
@@ -54,8 +57,8 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
         TODO("Not yet implemented")
     }
 
-    override fun onItemClick(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        var item:String = adapter?.getItemAtPosition(position).toString()
-        Log.i(TAG, item)
-    }
+//    override fun onItemClick(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//        var item:String = adapter?.getItemAtPosition(position).toString()
+//        Log.i(TAG, item)
+//    }
 }
